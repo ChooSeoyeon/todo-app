82cb4b7f4fc870fb513148d57f2e09925262f82d
Choo <choo000407@naver.com>
2022-02-07 08:12:37 +0900

    feat: Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ MongoClient.connect('mongodb+srv://admin:qwer4321@cluster0.u0p2o.mongodb.net/tod
     if (에러) return console.log(에러);
 
     db = client.db('todoapp');
-    // db.collection('post').insertOne( {이름 : 'Choo', _id:1} , function(에러, 결과){
+    // db.collection('post').insertOne( {이름 : 'Choo', _id:2} , function(에러, 결과){
 	//     console.log('저장완료'); 
 	// });
     app.listen(8080, function(){
@@ -20,13 +20,13 @@ MongoClient.connect('mongodb+srv://admin:qwer4321@cluster0.u0p2o.mongodb.net/tod
 
 // 누군가가 /pet 으로 방문을 하면..
 // pet관련된 안내문을 띄어주자
-app.get('/pet', function(요청, 응답){
-    응답.send('펫 용품 쇼핑할 수 있는 페이지입니다.');
-}); 
+// app.get('/pet', function(요청, 응답){
+//     응답.send('펫 용품 쇼핑할 수 있는 페이지입니다.');
+// }); 
 
-app.get('/beauty', function(요청, 응답){
-    응답.send('뷰티 용품 쇼핑할 수 있는 페이지입니다.');
-});
+// app.get('/beauty', function(요청, 응답){
+//     응답.send('뷰티 용품 쇼핑할 수 있는 페이지입니다.');
+// });
 
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname+'/index.html');
@@ -41,16 +41,20 @@ app.get('/write', function(요청, 응답){
 // 이 때, 'post'라는 이름을 가진 collection에 두 개 데이터를 저장하기
 app.post('/add', function(요청,응답){
     응답.send('전송완료'); // 빠지면 안됨. 브라우저 멈춤.
+    // console.log(요청.body);
     // console.log(요청.body.title);
     // console.log(요청.body.date);
     //DB에 저장해주세요
     db.collection('counter').findOne({name: '게시물개수'}, function(에러, 결과){
         console.log(결과.totalPost);
         var 총게시물개수 = 결과.totalPost;
-        db.collection('post').insertOne({_id: 총게시물개수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date}, function(){
+        db.collection('post').insertOne({_id: 총게시물개수+1, 제목: 요청.body.title, 날짜: 요청.body.date}, function(){
             console.log('저장완료');
+            //  +counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함.
+             db.collection('counter').updateOne({name: '게시물개수'}, { $inc : {totalPost:1} }, function(에러, 결과){
+                if (에러) { return console.log(에러) }
+             });
         });
-        // +counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함.
     });
 });
 
